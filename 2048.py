17feec855caebf6cc29e23f48ce2c2e0e9d77c2d
VladNegoita <vlad1negoita@gmail.com>
2022-01-10 12:42:35 +0200

    Code spacing

--- 2048.py
+++ 2048.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+
 class Board:
     bg_color={
         '2': '#eee4da',
@@ -40,15 +41,17 @@ class Board:
         self.merge=False
         self.moved=False
         self.score=0
+        
         for i in range(4):
             rows=[]
             for j in range(4):
                 l=Label(self.gameArea,text='',bg='azure4',
                 font=('calibri',22,'bold'),width=4,height=2)
                 l.grid(row=i,column=j,padx=7,pady=7)
-                rows.append(l);
+                rows.append(l)
             self.board.append(rows)
         self.gameArea.grid()
+
     def reverse(self):
         for ind in range(4):
             i=0
@@ -57,8 +60,10 @@ class Board:
                 self.gridCell[ind][i],self.gridCell[ind][j]=self.gridCell[ind][j],self.gridCell[ind][i]
                 i+=1
                 j-=1
+
     def transpose(self):
         self.gridCell=[list(t)for t in zip(*self.gridCell)]
+
     def compressGrid(self):
         self.compress=False
         temp=[[0] *4 for i in range(4)]
@@ -71,6 +76,7 @@ class Board:
                         self.compress=True
                     cnt+=1
         self.gridCell=temp
+
     def mergeGrid(self):
         self.merge=False
         for i in range(4):
@@ -80,17 +86,19 @@ class Board:
                     self.gridCell[i][j + 1] = 0
                     self.score += self.gridCell[i][j]
                     self.merge = True
+
     def random_cell(self):
         cells=[]
         for i in range(4):
             for j in range(4):
                 if self.gridCell[i][j] == 0:
                     cells.append((i, j))
+
         curr=random.choice(cells)
         i=curr[0]
         j=curr[1]
         self.gridCell[i][j]=2
-    
+
     def can_merge(self):
         for i in range(4):
             for j in range(3):
@@ -102,6 +110,7 @@ class Board:
                 if self.gridCell[i+1][j] == self.gridCell[i][j]:
                     return True
         return False
+
     def paintGrid(self):
         for i in range(4):
             for j in range(4):
@@ -111,11 +120,13 @@ class Board:
                     self.board[i][j].config(text=str(self.gridCell[i][j]),
                     bg=self.bg_color.get(str(self.gridCell[i][j])),
                     fg=self.color.get(str(self.gridCell[i][j])))
+
 class Game:
     def __init__(self,gamepanel):
         self.gamepanel=gamepanel
         self.end=False
         self.won=False
+
     def start(self):
         self.gamepanel.random_cell()
         self.gamepanel.random_cell()
@@ -126,6 +137,7 @@ class Game:
     def link_keys(self,event):
         if self.end or self.won:
             return
+
         self.gamepanel.compress = False
         self.gamepanel.merge = False
         self.gamepanel.moved = False
@@ -160,8 +172,10 @@ class Game:
             self.gamepanel.reverse()
         else:
             pass
+
         self.gamepanel.paintGrid()
         print(self.gamepanel.score)
+
         flag=0
         for i in range(4):
             for j in range(4):
@@ -173,20 +187,23 @@ class Game:
             messagebox.showinfo('2048', message='Game WON!!! =))')
             print("won")
             return
+
         for i in range(4):
             for j in range(4):
                 if self.gamepanel.gridCell[i][j]==0:
                     flag=1
                     break
+
         if not (flag or self.gamepanel.can_merge()):
             self.end=True
             messagebox.showinfo('2048','Game Over!!! =((')
             print("Over")
+
         if self.gamepanel.moved:
             self.gamepanel.random_cell()
         
         self.gamepanel.paintGrid()
     
 gamepanel =Board()
-game2048 = Game( gamepanel)
+game2048 = Game(gamepanel)
 game2048.start()
